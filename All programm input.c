6c72dabba1f302b105a3d3470706c2dc954b8ce5
Tasnimul1 <tasnimul1009@gmail.com>
2024-07-30 21:38:32 +0600

    Tasnimul code

---
+++ All programm input.c
@@ -0,0 +1,182 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+//A programm to calculate if and else:
+/*int m;
+printf("Enter the number: ");
+scanf("%d",&m);
+
+
+if(80<=m&&m<=100)
+ {
+  printf("You got A+");
+ }
+else if(70<=m&&m<=79)
+{
+ printf("You got A");
+}
+else if(60<=m&&m<=69)
+{
+ printf("B+");
+
+}
+
+else if(50<=m&&m<=59)
+{
+printf("You got B");
+}
+
+else if(40<=m&&m<=49)
+
+{
+printf("You got C+");
+
+}
+else if(0<=m&&m<=39)
+{
+printf("You are falied");
+}
+else
+{
+printf("invaild input");
+}
+*/
+
+
+//A programm to calculate power:
+/*int base,power,result;
+printf("Enter the value of base:  ");
+scanf("%d",&base);
+printf("Enter the value of power: ");
+scanf("%d",&power);
+
+result=pow(base,power);
+printf("The result is %d",result);
+*/
+
+//A programm to calculate bazar:
+/*float fish,beef,rice,total,Gamount,Ramount;
+printf("Enter the price of fish: ");
+scanf("%f",&fish);
+printf("Enter the price of beef: ");
+scanf("%f",&beef);
+printf("Enter the price of rice: ");
+scanf("%f",&rice);
+
+total=fish+beef+rice;
+printf("Your total price is %.2f.\n",total);
+printf("Enter the Gamount: ");
+scanf("%f",&Gamount);
+
+Ramount=Gamount-total;
+printf("Your return amount is %f",Ramount);
+*/
+
+//A programm to calculate tringle:
+/*int base,height,area;
+printf("Enter the value of base:   ");
+scanf("%d",&base);
+printf("Enter the value of height: ");
+scanf("%d",&height);
+
+area=0.5*base*height;
+printf("The value is %d",area);
+*/
+
+//A programm to calculate degin:
+/*int degin,colour,total;
+printf("Enter the value of degin:  ");
+scanf("%d",&degin);
+printf("Enter the value of colour: ");
+scanf("%d",&colour);
+
+total=degin+colour;
+printf("The total is %d",total);
+*/
+
+//A programm to make it:
+
+/*int m,v,t,p,total;
+printf("Enter the value m: ");
+scanf("%d",&m);
+printf("Enter the value v: ");
+scanf("%d",&v);
+printf("Enter the value t: ");
+scanf("%d",&t);
+printf("Enter the value p: ");
+scanf("%d",&p);
+
+total=m+v+t+p;
+printf("The value is %d",total);
+*/
+
+//A programm to input to output:
+/*int a,b,sum;
+printf("Enter the value of a: ");
+scanf("%d",&a);
+printf("Enter the value of b: ");
+scanf("%d",&b);
+
+sum=a+b;
+printf("The value is %d.",sum);
+*/
+
+//A programm to calculate power:
+/*int base,power,result;
+printf("Enter the value of base: ");
+scanf("%d",&base);
+printf("Enter the value of power: ");
+scanf("%d",&power);
+result=pow(base,power);
+printf("The result is %d",result);
+*/
+
+//A programm to calculate vag gun:
+/*int quotient,remainder,divisor,divided;
+printf("Enter the value of divided: ");
+scanf("%d",&divided);
+printf("Enter the value of divisor: ");
+scanf("%d",&divisor);
+
+quotient=divided/divisor;
+remainder=divided%divisor;
+printf("The quotient is :  %.2d.\n",quotient);
+printf("The remiander is: %.2d.\n",remainder);
+*/
+
+//A programm to determine if else:
+/*int c;
+printf("Enter a number: ");
+scanf("%d",&c);
+
+if(c%2==1)
+{
+printf("%d is Odd number.",c);
+}
+else
+{
+printf("%d is even number.",c);
+}
+*/
+
+/*char a;
+int total,kg;
+
+printf("Enter your fruit name: ");
+scanf("%c",&a);
+
+if(a=='apple')
+{
+printf("How many kg?\n");
+scanf("%d",&kg);
+total=300*kg;
+printf("The price is %d",total);
+}
+*/
+
+
+return 0;
+
+}
+
