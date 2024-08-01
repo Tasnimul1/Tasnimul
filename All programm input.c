e23c3aba919dc6cc43455460eb1c14e27869de1e
Tasnimul1 <tasnimul1009@gmail.com>
2024-08-01 20:16:41 +0600

    C programms
    I am beginners in c.

--- All programm input.c
+++ All programm input.c
@@ -2,8 +2,8 @@
 #include<math.h>
 int main()
 {
-//A programm to calculate if and else:
-/*int m;
+//A programm to determine grade:
+int m;
 printf("Enter the number: ");
 scanf("%d",&m);
 
@@ -41,22 +41,22 @@ else
 {
 printf("invaild input");
 }
-*/
+
 
 
 //A programm to calculate power:
-/*int base,power,result;
+int base,power,result;
 printf("Enter the value of base:  ");
 scanf("%d",&base);
 printf("Enter the value of power: ");
 scanf("%d",&power);
 
 result=pow(base,power);
-printf("The result is %d",result);
-*/
+printf("The result is %.2d.\n",result);
 
-//A programm to calculate bazar:
-/*float fish,beef,rice,total,Gamount,Ramount;
+
+//A programm to calculate price:
+float fish,beef,rice,total,Gamount,Ramount;
 printf("Enter the price of fish: ");
 scanf("%f",&fish);
 printf("Enter the price of beef: ");
@@ -70,34 +70,23 @@ printf("Enter the Gamount: ");
 scanf("%f",&Gamount);
 
 Ramount=Gamount-total;
-printf("Your return amount is %f",Ramount);
-*/
+printf("Your return amount is %.2f.\n",Ramount);
 
-//A programm to calculate tringle:
-/*int base,height,area;
+
+//A programm to calculate area of tringle:
+int base,height,area;
 printf("Enter the value of base:   ");
 scanf("%d",&base);
 printf("Enter the value of height: ");
 scanf("%d",&height);
 
 area=0.5*base*height;
-printf("The value is %d",area);
-*/
+printf("The value is %d\n",area);
 
-//A programm to calculate degin:
-/*int degin,colour,total;
-printf("Enter the value of degin:  ");
-scanf("%d",&degin);
-printf("Enter the value of colour: ");
-scanf("%d",&colour);
-
-total=degin+colour;
-printf("The total is %d",total);
-*/
 
 //A programm to make it:
 
-/*int m,v,t,p,total;
+int m,v,t,p,total;
 printf("Enter the value m: ");
 scanf("%d",&m);
 printf("Enter the value v: ");
@@ -108,45 +97,45 @@ printf("Enter the value p: ");
 scanf("%d",&p);
 
 total=m+v+t+p;
-printf("The value is %d",total);
-*/
+printf("The value is %d\n",total);
 
-//A programm to input to output:
-/*int a,b,sum;
+
+//A programm to add two numbers:
+int a,b,sum;
 printf("Enter the value of a: ");
 scanf("%d",&a);
 printf("Enter the value of b: ");
 scanf("%d",&b);
 
 sum=a+b;
-printf("The value is %d.",sum);
-*/
+printf("The value is %.2d.\n",sum);
+
 
 //A programm to calculate power:
-/*int base,power,result;
+int base,power,result;
 printf("Enter the value of base: ");
 scanf("%d",&base);
 printf("Enter the value of power: ");
 scanf("%d",&power);
 result=pow(base,power);
 printf("The result is %d",result);
-*/
 
-//A programm to calculate vag gun:
-/*int quotient,remainder,divisor,divided;
-printf("Enter the value of divided: ");
-scanf("%d",&divided);
+
+//A programm to calculate quotient&remainder:
+int quotient,remainder,divisor,dividend;
+printf("Enter the value of dividend: ");
+scanf("%d",&dividend);
 printf("Enter the value of divisor: ");
 scanf("%d",&divisor);
 
-quotient=divided/divisor;
-remainder=divided%divisor;
+quotient=dividend/divisor;
+remainder=dividend%divisor;
 printf("The quotient is :  %.2d.\n",quotient);
 printf("The remiander is: %.2d.\n",remainder);
-*/
 
-//A programm to determine if else:
-/*int c;
+
+//A programm to determine odd and even:
+int c;
 printf("Enter a number: ");
 scanf("%d",&c);
 
@@ -158,22 +147,8 @@ else
 {
 printf("%d is even number.",c);
 }
-*/
 
-/*char a;
-int total,kg;
 
-printf("Enter your fruit name: ");
-scanf("%c",&a);
-
-if(a=='apple')
-{
-printf("How many kg?\n");
-scanf("%d",&kg);
-total=300*kg;
-printf("The price is %d",total);
-}
-*/
 
 
 return 0;
